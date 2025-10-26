7b903a468fee713b310b777624ca28ed505536ce
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2025-10-26 16:55:34 -0400

    Add ROS2

---
+++ slides/ros-arm-position-control/code/ros2/moveit_demo.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+MoveIt Commander Demo for ROS Arm Position Control
+"""
+
+import rospy
+import moveit_commander
+import geometry_msgs.msg
+from math import pi
+import sys
+
+class MoveItDemo:
+    def __init__(self):
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node('moveit_demo', anonymous=True)
+        
+        self.robot = moveit_commander.RobotCommander()
+        self.scene = moveit_commander.PlanningSceneInterface()
+        self.move_group = moveit_commander.MoveGroupCommander("manipulator")
+
+    def go_to_joint_state(self, joint_goal):
+        """関節角度を指定して移動"""
+        self.move_group.go(joint_goal, wait=True)
+        self.move_group.stop()
+        
+    def go_to_pose_goal(self, pose_goal):
+        """姿勢を指定して移動"""
+        self.move_group.set_pose_target(pose_goal)
+        plan = self.move_group.go(wait=True)
+        self.move_group.stop()
+        self.move_group.clear_pose_targets()
+        return plan
+
+    def demo_sequence(self):
+        # ホームポジションに移動
+        joint_goal = [0, -pi/4, 0, -pi/2, 0, pi/3]
+        self.go_to_joint_state(joint_goal)
+        
+        # 姿勢指定での移動
+        pose_goal = geometry_msgs.msg.Pose()
+        pose_goal.orientation.x = -1.0
+        # pose_goal.orientation.w = 1.0
+        pose_goal.position.x = 0.4
+        pose_goal.position.y = 0.1
+        pose_goal.position.z = 0.4
+        self.go_to_pose_goal(pose_goal)
+
+if __name__ == '__main__':
+    demo = MoveItDemo()
+    demo.demo_sequence()
